820d149d4373f89081bf1fea9f65fa06f611064d
EmilAliyev <emilaliyev222@yahoo.com>
2019-04-11 19:51:30 -0700

    Implemented slow worker command

--- Captain/Assets/Scripts/SlowWorkerPirateCommand.cs
+++ Captain/Assets/Scripts/SlowWorkerPirateCommand.cs
@@ -8,11 +8,62 @@ namespace Captain.Command
 {
     public class SlowWorkerPirateCommand : ScriptableObject, IPirateCommand
     {
-        //Work for 20-40s and produce 1 item every 8s. Default command for pirates.
+        private float totalWorkDuration; //Duration to work
+        private float totalWorkDone; //Total amount of work done
+        private float workSinceProduction; //Work done since last time an item was produced
+        private const float PRODUCTION_TIME = 8.0f; //Interval in which items are produced
+        private const float MIN_WORK_DURATION = 20.0f; //Minimum work duration
+        private const float MAX_WORK_DURATION = 40.0f; //Maximum work duration
+        private bool exhausted = false;
 
+        public SlowWorkerPirateCommand()
+        {
+            //Initialize variables
+            totalWorkDone = 0;
+            workSinceProduction = 0;
+
+            //Total duration: 20-40s (picked randomly)
+            //Get a Random.value, multiply by the difference between max and min duration to get the range,
+            //add the min duration to get the value (so if Random.value returns 0, totalWorkDuration = min, if
+            //1, totalWorkDuration = max)
+            totalWorkDuration = Random.value * (MAX_WORK_DURATION - MIN_WORK_DURATION) + MIN_WORK_DURATION;
+
+        }
+
+        //Check if work is done
+        private bool workDone()
+        {
+            if(totalWorkDone >= totalWorkDuration)
+            {
+                exhausted = true;
+                return true;
+            }
+            return false;
+        }
+
+        //Produce item if interval is up
+        public void produceItem(GameObject pirate, Object itemPrefab)
+        {
+            //If interval is up
+            if (workSinceProduction >= PRODUCTION_TIME)
+            {
+                workSinceProduction = 0;
+
+                //Create item
+                var item = (GameObject)Instantiate(itemPrefab, pirate.transform);
+            }
+        }
+
+        //Work for 20-40s and produce 1 item every 8s. Default command for pirates.
         public bool Execute(GameObject pirate, Object productPrefab)
         {
-            throw new System.NotImplementedException();
+            //Add time since last frame to work done
+            totalWorkDone += Time.deltaTime;
+            workSinceProduction += Time.deltaTime;
+
+            produceItem(pirate, productPrefab);
+            return workDone();
+            
         }
     }
 
